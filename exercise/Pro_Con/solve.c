21823fa8d054bdbede51b03f3e51231b48b87ee7
31377 <3137753560@qq.com>
2025-11-06 20:30:12 +0800

    更新共享内存示例程序，添加随机数生成函数并获取用户输入的最小值和最大值

--- exercise/Pro_Con/solve.c
+++ exercise/Pro_Con/solve.c
@@ -1,12 +1,31 @@
-#include "comm.h"
+#include "../../my_basic/semaphore/comm.h"
 #include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
     pid_t pid; // 定义进程号
     int shmid;
     char *shmaddr;
+    int min, max;
 
+    /*定义生成随机数函数*/
+    printf("请输入最小值和最大值:\n");
+    scanf("%d %d", &min, &max);
+    long rand_in_range(int min, int max)
+    {
+        // 确保范围有效
+        if (min > max)
+        {
+            int temp = min;
+            min = max;
+            max = temp;
+        }
+        // 生成 [min, max] 之间的随机数
+        return min + (random() % (max - min + 1));
+    }
     /* 1、创建共享内存并映射共享内存空间 */
     shmid = ShmInit(".", 65, 1024, &shmaddr);
     if (shmid == -1)
@@ -27,6 +46,12 @@ int main(int argc, char const *argv[])
     else if (pid == 0)
     {
         // 子进程循环写入100个整数
+        int i = 0;
+        int count = 100;
+
+        for (int i = 0; i < count; i++)
+        {
+        }
     }
 
     return 0;
